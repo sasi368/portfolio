988027211a7d55e15467ab68cf651fce16f91718
sasikumar <sasimdu.murugan@gmail.com>
2025-02-18 12:11:18 +0530

    latest changes

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -1,40 +1,46 @@
+import { useEffect, useState } from "react";
+import axios from "axios"; // Import axios
 import { Container } from "./styles";
 import ScrollAnimation from "react-animate-on-scroll";
 import Images from "../../assets/images";
-import { projects } from "../../config";
+import { projectsDetails } from "../../config";
 
 export function Project() {
   return (
     <Container id="project">
       <h2>Some Working Projects</h2>
       <div className="projects">
-        {projects?.map((item, index) => {
+        {projectsDetails?.map((item: any, index: any) => {
           return (
-            <ScrollAnimation animateIn="flipInX">
+            <ScrollAnimation animateIn="flipInX" key={index}>
               <div className="project">
                 <header>
                   <div className="project-links">
-                    <a href={item.playstore} target="_blank" rel="noreferrer">
-                      <img src={Images.playstore} alt="Visit site" />
-                    </a>
-                    {item.appstore != "" && (
+                    {item.playstore && (
+                      <a href={item.playstore} target="_blank" rel="noreferrer">
+                        <img src={Images.playstore} alt="Visit site" />
+                      </a>
+                    )}
+                    {item.appstore && (
                       <a href={item.appstore} target="_blank" rel="noreferrer">
                         <img src={Images.applewhite} alt="Visit site" />
                       </a>
                     )}
-                    <a href={item.site} target="_blank" rel="noreferrer">
-                      <img src={Images.externalLink} alt="Visit site" />
-                    </a>
+                    {item.site && (
+                      <a href={item.site} target="_blank" rel="noreferrer">
+                        <img src={Images.externalLink} alt="Visit site" />
+                      </a>
+                    )}
                   </div>
                 </header>
                 <div className="body">
                   <h3>{item.title}</h3>
-                  <p> {item.description}</p>
+                  <p>{item.description}</p>
                 </div>
                 <footer>
                   <ul className="tech-list">
-                    {item.techUses.map((tecItem, tecIndex) => {
-                      return <li>{tecItem}</li>;
+                    {item.techUses.map((tecItem: any, tecIndex: number) => {
+                      return <li key={tecIndex}>{tecItem}</li>;
                     })}
                   </ul>
                 </footer>
--- src/config.js
+++ src/config.js
@@ -10,7 +10,7 @@ const links = {
   tech: {
     jobRole: "Mobile App Developer",
     resumeLink:
-      "https://drive.google.com/file/d/1bfQHj-2-sZLmNtjlfzWiwX6TQwN39DWj/view?usp=sharing",
+      "https://drive.google.com/file/d/1VkSy-erZwxJ6I4P-eIzGifBiDPpvL5P7/view?usp=sharing",
     noOfExperience: "3+",
     github: "https://github.com/sasi368",
   },
@@ -28,11 +28,11 @@ const contents = {
   },
 };
 
-const projects = [
+const projectsDetails = [
   {
     title: "Decentralized Crypto Wallet with Multi-Network Support",
     description:
-      "Designed and developed a decentralized cryptocurrency wallet similar to Trust Wallet, supporting all EVM-compatible networks alongside BTC, LTC, and XRP. Integrated advanced features like ParaSwap and Stargate Swap for seamless token swapping, staking capabilities, and an engaging game module with prediction and spin wheel functionalities. Leveraged blockchain technology to deliver a secure, scalable, and user-friendly solution for managing digital assets.",
+      "Designed and developed a decentralized cryptocurrency wallet similar to Trust Wallet, supporting all EVM-compatible networks alongside BTC, LTC, and XRP. Own Dapp browser, Integrated advanced features like ParaSwap and Stargate Swap for seamless token swapping, staking capabilities, and an engaging game module with prediction and spin wheel functionalities. Leveraged blockchain technology to deliver a secure, scalable, and user-friendly solution for managing digital assets.",
     playstore:
       "https://play.google.com/store/apps/details?id=com.b5wallet&pcampaignid=web_share",
     appstore: "",
@@ -44,6 +44,17 @@ const projects = [
       "Moralis",
       "bitcoin-core",
     ],
+  },
+
+  {
+    title: "Event Management and Invitation App Development",
+    description:
+      "Developed an innovative event management app designed to streamline the process of creating and sending personalized digital invitations. The app features a user-friendly interface with customizable templates, enabling users to design creative and unique invitations effortlessly. By offering the option to send invitations via SMS, the app enhances the personal touch, encouraging higher attendance and demonstrating genuine care for the event. This project showcases expertise in crafting engaging and functional mobile applications tailored for social and professional gatherings.",
+    playstore:
+      "https://play.google.com/store/apps/details?id=com.invajt&pcampaignid=web_share",
+    appstore: "https://apps.apple.com/se/app/invajt/id6471048773?l=en-GB",
+    site: "https://invajt.com/",
+    techUses: ["React Native", "node.js", "mongodb"],
   },
 
   {
@@ -59,14 +70,49 @@ const projects = [
   },
 
   {
-    title: "Event Management and Invitation App Development",
+    title: "Taxi Booking - Customer App",
     description:
-      "Developed an innovative event management app designed to streamline the process of creating and sending personalized digital invitations. The app features a user-friendly interface with customizable templates, enabling users to design creative and unique invitations effortlessly. By offering the option to send invitations via SMS, the app enhances the personal touch, encouraging higher attendance and demonstrating genuine care for the event. This project showcases expertise in crafting engaging and functional mobile applications tailored for social and professional gatherings.",
-    playstore:
-      "https://play.google.com/store/apps/details?id=com.invajt&pcampaignid=web_share",
-    appstore: "https://apps.apple.com/se/app/invajt/id6471048773?l=en-GB",
-    site: "https://invajt.com/",
-    techUses: ["React Native", "node.js", "mongodb"],
+      "The customer app offers a convenient and user-friendly platform for booking taxis on demand. Users can quickly request rides, track drivers in real-time, and securely pay within the app. The app also allows users to view ride history, manage their profiles, and provide ratings and feedback for completed trips. Designed to simplify transportation, it ensures a seamless experience for customers.",
+    playstore: "",
+    appstore: "",
+    site: "",
+    techUses: ["React Native", "Laravel", "My Sql", "", "Google Maps API"],
+  },
+
+  {
+    title: "Taxi Service Management - Driver App",
+    description:
+      "The driver app is tailored to help taxi drivers efficiently manage ride requests. Drivers can accept or decline bookings, navigate to pickup and drop-off locations using GPS, and track their earnings in real-time. The app includes features such as trip history, profile management, and in-app communication with passengers to ensure smooth and professional operations. It empowers drivers with the tools they need to provide reliable taxi services.",
+    playstore: "",
+    appstore: "",
+    site: "",
+    techUses: ["React Native", "Laravel", "My Sql", "", "Google Maps API"],
+  },
+
+  {
+    title: "Fantasy Sports - Dream11 Clone App",
+    description:
+      "The Dream11 clone app is a feature-rich fantasy sports platform that allows users to create their own virtual teams, join contests, and win real rewards. With real-time match updates, secure payment integration, and an intuitive user interface, the app delivers an engaging experience for sports enthusiasts. Users can participate in multiple leagues, track their performance, and enjoy seamless gameplay with advanced analytics and leaderboards.",
+    playstore: "",
+    appstore: "",
+    site: "",
+    techUses: [
+      "React Native",
+      "Node.js",
+      "MongoDB",
+      "Firebase",
+      "Razorpay API",
+    ],
+  },
+
+  {
+    title: "NFT Creator and Manager",
+    description:
+      "An advanced NFT application designed for creating and managing NFTs on Ethereum (ETH) and Binance Smart Chain (BSC) networks. Users can create both single (ERC-721) and multiple (ERC-1155) NFTs effortlessly. The app also allows users to transfer NFTs to other wallets seamlessly, ensuring a smooth and secure experience.",
+    techUses: ["React Native", "Node.js", "Ethers.js", "MongoDB"],
+    playstore: "",
+    appstore: "",
+    site: "",
   },
 ];
-export { links, contents, projects };
+export { links, contents, projectsDetails };
